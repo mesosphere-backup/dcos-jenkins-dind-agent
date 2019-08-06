ecc7119717f4ee426ad9319ed5358c6a776d967c
Bryan Latten <latten@adobe.com>
2019-08-06 17:02:30 -0400

    Dockerfile: updating dind engine to 19.03
    - Suppressing horrific engine logs

--- wrapper.sh
+++ wrapper.sh
@@ -2,12 +2,15 @@
 set -e
 
 echo "==> Launching the Docker daemon..."
-dind dockerd --storage-driver=overlay $DOCKER_EXTRA_OPTS &
+dind dockerd --storage-driver=overlay $DOCKER_EXTRA_OPTS 2>1 &
 
 while(! docker info > /dev/null 2>&1); do
     echo "==> Waiting for the Docker daemon to come online..."
     sleep 1
 done
 echo "==> Docker Daemon is up and running!"
+docker version
+echo "==> Launcher complete"
+echo ""
 
 /bin/sh -c "$@"
