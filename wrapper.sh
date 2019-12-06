1e577e648bf755cbfa3c31dd236a6ba8fcdfd801
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-12-05 23:01:25 -0800

    Add --experimental flag to support docker squash.

--- wrapper.sh
+++ wrapper.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "==> Launching the Docker daemon..."
-dind dockerd --storage-driver=overlay $DOCKER_EXTRA_OPTS &
+dind dockerd --storage-driver=overlay --experimental $DOCKER_EXTRA_OPTS &
 
 while(! docker info > /dev/null 2>&1); do
     echo "==> Waiting for the Docker daemon to come online..."
